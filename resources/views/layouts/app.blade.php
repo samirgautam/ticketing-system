b6d102044ab735bfc11f7707b413dd607cbfa2c6
sonugoswami <sonugoswami98622@gmail.com>
2022-12-11 12:58:52 +0545

    tt1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
     <footer>
      <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2023 Copyright:
+    © 2083 Copyright:
     <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
   </div>
   <!-- Copyright -->
