e9179c3558f20d4d3e7d8e804ccf18304604d45d
Anish Shrestha <077bct013@ioepc.edu.np>
2022-12-11 12:56:38 +0545

    TEST!!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
     <footer>
      <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
+    © 2024 Copyright:
     <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
   </div>
   <!-- Copyright -->
