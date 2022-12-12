92bbb0e159198bfa90f798d8bdc6adc44a4c6cef
Anish Shrestha <077bct013@ioepc.edu.np>
2022-12-12 08:18:04 +0545

    added: iticket cons on 'ticket.list'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,9 @@
    
 >>>>>>> 23a5a2db097b644760f1c0837cdac2a8efeaebfa
 
+//font awesome
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+
 </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
